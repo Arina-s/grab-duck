cf73b3b6669ea01528595f2b195d3a080cf106d3
Arina Slizh <arina_slizh@epam.com>
2021-12-16 08:49:24 +0300

    Initial commit

---
+++ src/main/java/com/grabduck/grabduck/GrabDuckApplication.java
@@ -0,0 +1,13 @@
+package com.grabduck.grabduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabDuckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrabDuckApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/grabduck/grabduck/HelloController.java
@@ -0,0 +1,14 @@
+package com.grabduck.grabduck;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "hello";
+    }
+
+}
